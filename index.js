265a2815a32ed7a3278b2601d8ae973f0c9c86d0
Lusani Mphigalale <lusani.22mphigalale@gmail.com>
2026-09-07 20:55:09 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function addCart(event){
+    const button = event.currentTarget;
+    
+    if(button.innerHTML.includes('bi-bag-plus')){
+        button.innerHTML = '<i class="bi bi-check-circle"></i>';
+        alert("Added to cart");
+    }else{
+        button.innerHTML = '<i class="bi bi-bag-plus"></i>';
+        alert("Item Removed");
+    }
+}
+
+document.querySelectorAll('.btn').forEach(btn => {
+    btn.addEventListener('click', addCart);
+});
+
